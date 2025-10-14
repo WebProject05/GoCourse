5e4248971a4c9275473b376441740b9edf0b6d49
Chopperla Santosh <chopperla2005@gmail.com>
2025-10-14 22:28:51 +0530

    add strings

--- Intermediate/string.go
+++ Intermediate/string.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
 func main() {
-	
-}
+	message := "hello \nmate"
+	message2 := "hello \tmate"
+
+	fmt.Println(message)
+	fmt.Println(message2)
+
+
+	fmt.Println("The lenght of the message is", len(message))
+	fmt.Println("The lenght of the message is", len(message2))
+
+	fmt.Println("FIrst character in the message ",message[0])   // ASCII
+
+	first := "San"
+	second := "tosh"
+
+	full := first + second
+	fmt.Println(full)
+
+	for i, char := range message {
+		fmt.Printf("%d %c\n", i, char)
+	}
+
+	fmt.Println("Rune count: ", utf8.RuneCountInString(full))
+
+	var cha rune = '₹'
+	fmt.Println(cha)
+	fmt.Printf("\n %c", cha)
+
+	convertedStr := string(cha)
+	fmt.Printf("Type of converted: %T \n", convertedStr)
+	fmt.Printf("Type of converted: %T \n", cha)
+
+	helloT := "హలో"
+	fmt.Println(helloT)
+	for _, runeValue := range helloT {
+		fmt.Printf("%c \n", runeValue)
+	}
+} 
