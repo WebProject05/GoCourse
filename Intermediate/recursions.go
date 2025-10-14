6069e994814e795ba633b4ddc360c5e02d038141
Chopperla Santosh <chopperla2005@gmail.com>
2025-10-14 21:29:18 +0530

    add recursions

--- Intermediate/recursions.go
+++ Intermediate/recursions.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(factorial(10))
+	fmt.Println(sumOfDigits(398095087))
+}
+
+func factorial(n int) int {
+	// base case should be the main prioriyt (for my kn)
+	if n == 0 {
+		return 1
+	}
+
+	// recursive case
+	return n * factorial(n-1)
+}
+
+
+
+func sumOfDigits(num int) int {
+	if num < 10 {
+		return num
+	}
+
+	return num % 10 + sumOfDigits(num / 10)
 }
