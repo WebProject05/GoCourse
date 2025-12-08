afd3c10ffac55cb48a9e9d0810da351cb514a7ba
Chopperla Santosh <chopperla2005@gmail.com>
2025-12-08 16:54:11 +0530

    updated channels

--- Advanced/channels.go
+++ Advanced/channels.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+// Channels are used for communication between concurrent Goroutines
+// Helps synchronize and manage the flow of data in concurrent programs
+
+
+
+func main() {
+	//variable := make(chan type), '<-' recive operator
+	greeting := make(chan string)
+	greetString := "Hello"
+
+	go func() {
+		greeting <- greetString   //Sending the value of greetString to greeting channel		
+		// fmt.Println(greeting)  This is a channel address
+		greeting <- "Hello"  // To read this value we have to read again
+	}()
+
+	receiver := <- greeting   // receiver is receving value from the greeting channel
+	fmt.Println(receiver)
+	receiver = <- greeting
+	fmt.Println(receiver)
+	fmt.Println("End of end")
 }
