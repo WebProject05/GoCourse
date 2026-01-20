2974b817f8e2c9dbcf7e42c13fb6218cc2c17bbc
Chopperla Santosh <chopperla2005@gmail.com>
2026-01-20 23:18:59 +0530

    Modified CVP

--- Concurrency/ConcVsPara.go
+++ Concurrency/ConcVsPara.go
@@ -1,16 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
 
+// func printElements() {
+// 	for i := range 5 {
+// 		fmt.Println(i)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
 
+// func printLetters() {
+// 	for _, val := range "ABCED" {
+// 		fmt.Println(string(val))
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
 
+func heavyTask(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Task %d is starting \n", id)
+	for range 100_000_000 {
 
-func printElements() {
-	for i := range 5 {
-		fmt.Println(i)
 	}
+	fmt.Println((time.Now()))
+	fmt.Printf("Task %d is completed \n", id)
 }
 
 func main() {
-	printElements()
+	// go printElements()
+	// go printLetters()
+
+	// time.Sleep(3 * time.Second)
+	numThreads := 4
+
+	runtime.GOMAXPROCS(numThreads)
+	var wg sync.WaitGroup
+	for i := range numThreads {
+		wg.Add(1)
+		go heavyTask(i, &wg)
+	}
+
+	wg.Wait()
 }
